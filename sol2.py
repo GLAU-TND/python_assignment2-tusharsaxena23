b37ffa3081eedf931541a3166d2d6658588d840e
tusharsaxena23 <36040883+tusharsaxena23@users.noreply.github.com>
2019-10-13 22:02:51 +0530

    Add files via upload

---
+++ sol2.py
@@ -0,0 +1,16 @@
+l=eval(input())
+k=int(input())
+c=0
+for i in range(len(l)):
+    a=0
+    for j in range(len(l)):
+        if j>i:
+            a+=l[j]
+            if a>k:
+                break
+            if a==k:
+                print(l[i+1:j+1])
+                c=1
+    if c==1:
+        break
+            
