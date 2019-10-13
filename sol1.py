ffc0b8f9761ee78019fe9573aa10cbfb67a152a4
tusharsaxena23 <36040883+tusharsaxena23@users.noreply.github.com>
2019-10-13 22:15:57 +0530

    Update sol1.py

--- sol1.py
+++ sol1.py
@@ -2,11 +2,18 @@ s=list(input().split())
 k=int(input())
 c=9
 i=0
-while i!=len(s):
-    if len(s[i])<=c:
-        print(s[i],end=" ")
-        c=c-len(s[i])
-        i+=1
-    else:
-        print()
-        c=9
+k=0
+for p in s:
+    if len(p)>10:
+        k+=1
+if k==0:
+    while i!=len(s):
+        if len(s[i])<=c:
+            print(s[i],end=" ")
+            c=c-len(s[i])
+            i+=1
+        else:
+            print()
+            c=9
+else:
+    print(None)
